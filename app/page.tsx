a6a65beae78ca611e823809ebd7e4674c3f1d727
ShanaShin <154096641+ShanaShin@users.noreply.github.com>
2024-08-14 12:30:52 -0400

    Main Structure and installed supabase in env

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,28 @@ export default function Home() {
   ];
   return (
     <main className="min-h-screen mx-auto max-w-[100rem]">
-      {products.map((product) => (
-        <Card key={`${product.name}-${product.id}`} {...product}/>
-      ))}
+      <div className="px-12 pt-12 pb-20">
+        <div className="flex flex-col xl:flex-row gap-2 xl:gap-40">
+          <div className="pt-12">
+            <h2 className="text-4xl mb-16">OUR TOP PRODUCTS</h2>
+            <p className="text-xl">You can pay to boost your products here.</p>
+          </div>
+          <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-2 xl:gap-12">
+            {products.map((product) => (
+              <Card key={`${product.name}-${product.id}`} {...product}/>
+            ))}
+          </div>
+        </div>
+
+        <h2 className="text-4xl mt-20 mb-16">ALL PRODUCTS</h2>
+        {/* {products && products.length > 0 ? ( */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+            {products.map((product) => (
+              <Card key={`${product.name}-${product.id}`} {...product}/>
+            ))}
+          </div>
+        </div>
+      
     </main>
   );
 }
