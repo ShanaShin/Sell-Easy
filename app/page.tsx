1e33affd4da02b4f5e2cfbc3506c36a32850d239
ShanaShin <154096641+ShanaShin@users.noreply.github.com>
2024-08-14 13:36:39 -0400

    Fetched Data w Supabase For Products
    (fetched, but the images don't load properly)

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,22 @@
 import Card from "@/components/card";
-import Image from "next/image";
+import { createClient } from "@/supabase/client";
 
-export default function Home() {
-  const products = [
-    {
-      id: 0,
-      name: "Mushroom Orange Lamp",
-      description: "mol desc",
-      price: 40,
-      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9FNmOZEQc8tOOCO8BXxlyfGnGDiS8v_llDw&s",
-    },
-  ];
+export default async function Home() {
+  const supabase = createClient();
+  // const products = [
+  //   {
+  //     id: 0,
+  //     name: "Mushroom Orange Lamp",
+  //     description: "mol desc",
+  //     price: 40,
+  //     imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9FNmOZEQc8tOOCO8BXxlyfGnGDiS8v_llDw&s",
+  //   },
+  // ];
+  const { data: products, error } = await supabase.from("selleasy-products").select()
+  
+  if (!products) {
+    return <p>Not found</p>;
+  }
   return (
     <main className="min-h-screen mx-auto max-w-[100rem]">
       <div className="px-12 pt-12 pb-20">
@@ -21,7 +27,8 @@ export default function Home() {
           </div>
           <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-2 xl:gap-12">
             {products.map((product) => (
-              <Card key={`${product.name}-${product.id}`} {...product}/>
+              <Card key={`${product.name}-${product.id}`} {...product}
+              imageUrl={'/${process.env.SUPABASE_URL}/storage/v1/object/public/storage${product.imageUrl}'}/>
             ))}
           </div>
         </div>
@@ -30,7 +37,8 @@ export default function Home() {
         {/* {products && products.length > 0 ? ( */}
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
             {products.map((product) => (
-              <Card key={`${product.name}-${product.id}`} {...product}/>
+              <Card key={`${product.name}-${product.id}`} {...product}
+              imageUrl={'/${process.env.SUPABASE_URL}/storage/v1/object/public/storage${product.imageUrl}'}/>
             ))}
           </div>
         </div>
